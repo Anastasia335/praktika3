a5569646941e36e0ea9a1658568896319ebe2068
Anastasia <super.okay335@yandex.ru>
2022-08-31 21:22:35 +0300

    Добавили решение первой задачи

--- example001/Program.cs
+++ example001/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.WriteLine("ВВедите число А");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("ВВедите число B");
+int b = Convert.ToInt32(Console.ReadLine());
+int stepen = a;
+for (int i = 1; i<b; i++)
+{
+stepen = stepen * a;
+}
+Console.WriteLine ("А в степени В равно : " + stepen);
