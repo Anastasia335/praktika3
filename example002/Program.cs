86b17fd548bd7f33ec781551354c1934b822202f
Anastasia <super.okay335@yandex.ru>
2022-09-01 19:58:38 +0300

    Добавили решение второй задачи

--- example002/Program.cs
+++ example002/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+string size = Convert.ToString(a);
+int index = size.Length;
+int sum = 0;
+int prom = 0;
+for (int i = 0; i < index; i++)
+{
+   prom = a - a % 10;
+   sum = sum + (a - prom);
+   a = a / 10;
+}
+Console.WriteLine(sum);
